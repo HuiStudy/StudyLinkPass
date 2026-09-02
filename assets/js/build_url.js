08c08e695a7e00c0e652582b7f65177637113d79
HuiStudy <79929708@qq.com>
2026-09-02 14:07:22 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -4,9 +4,9 @@ function build_url(){
         document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
     }
     else {
-         // 先对 URL 做 encodeURIComponent（确保 UTF-8 安全），再 Base64 编码
-    var encoded = btoa(encodeURIComponent(url));
-    var fullLink = document.location.href + "api/?url=" + encoded;
-    document.getElementById("b_url").innerHTML = '<a href="' + fullLink + '" target="_blank">' + fullLink + '</a>';
+        // 先对 URL 做 encodeURIComponent（确保 UTF-8 安全），再 Base64 编码
+        var encoded = btoa(encodeURIComponent(url));
+        var fullLink = document.location.href + "api/?url=" + encoded;
+        document.getElementById("b_url").innerHTML = '<a href="' + fullLink + '" target="_blank">' + fullLink + '</a>';
     }
 }
